dfb063767c75e46a9121c45e3b3ff6c9c0fc2fd8
lukkio88 <luca.gagliano.1988@gmail.com>
2023-12-06 21:18:13 +0900

    Clean up and refactoring

--- OpenCL-CLHPP-Examples/vec2scalar/main.cpp
+++ OpenCL-CLHPP-Examples/vec2scalar/main.cpp
@@ -1,72 +1,20 @@
-#define CL_HPP_ENABLE_EXCEPTIONS
-#define CL_HPP_TARGET_OPENCL_VERSION 300
-
-#include <CL/opencl.hpp>
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <utils.h>
 
-void printVector(std::vector<float>& v)
-{
-    for (int i = 0; i < v.size(); ++i)
-    {
-        std::cout << v[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
-std::string readFile(const std::string& path)
-{
-    std::ifstream t(path);
-    std::stringstream buffer;
-    buffer << t.rdbuf();
-    return buffer.str();
-}
-
-void printPlatformsAndDevices(
+void vec2ScalarExample(
     const std::string& platformName = "NVIDIA CUDA",
     const cl_device_type& deviceType = CL_DEVICE_TYPE_GPU,
     const std::string& deviceName = "GeForce GT 730")
 {
-    std::vector<cl::Platform> platforms;
-    cl::Platform::get(&platforms);
-    cl::Platform platform;
 
-    for (auto& p : platforms) {
-        const std::string& currentPlatformName = p.getInfo<CL_PLATFORM_NAME>();
-        if (currentPlatformName == platformName)
-        {
-            std::cout << p.getInfo<CL_PLATFORM_NAME>() << std::endl;
-            platform = p;
-        }
-    }
-
-    std::vector<cl::Device> devices;
-    platform.getDevices(deviceType, &devices);
-    cl::Device device;
-    for (auto& d : devices)
-    {
-        const std::string& currentDeviceName = d.getInfo<CL_DEVICE_NAME>();
-        if (deviceName == currentDeviceName)
-        {
-            std::cout << d.getInfo<CL_DEVICE_NAME>() << std::endl;
-            device = d;
-        }
-    }
-
-    const cl::string& kernelSource = readFile(CL_KERNEL_PATH + std::string("/v2scalar.cl"));
-
-    cl::Context context(device);
-    cl::Device d = context.getInfo<CL_CONTEXT_DEVICES>()[0];
-    std::cout << d.getInfo<CL_DEVICE_NAME>() << std::endl;
-    cl::Program program(context, kernelSource);
-    auto buildResult = program.build(device);
-
-    if (buildResult != CL_SUCCESS)
-    {
-        std::cout << "Cannot build program" << std::endl;
-        exit(1);
-    }
+    OpenCLData setupData = basicSetup(
+        platformName, 
+        deviceType, 
+        deviceName, 
+        CL_KERNEL_PATH + std::string("/v2scalar.cl")
+    );
 
     const int DIM = 8;
 	std::vector<float> x;
@@ -76,12 +24,12 @@ void printPlatformsAndDevices(
         x.push_back(i + 1);
     }
 
-    cl::Buffer xBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * DIM * 2);
-    cl::Buffer tmpBuffer(context, CL_MEM_READ_WRITE, sizeof(float) * DIM * 2);
+    cl::Buffer xBuffer(setupData.context, CL_MEM_READ_ONLY, sizeof(float) * DIM * 2);
+    cl::Buffer tmpBuffer(setupData.context, CL_MEM_READ_WRITE, sizeof(float) * DIM * 2);
     std::vector<cl::Kernel> kernel = {
-        cl::Kernel(program,"calculateSum"),
-        cl::Kernel(program,"maxArray"),
-        cl::Kernel(program,"minArray")
+        cl::Kernel(setupData.program,"calculateSum"),
+        cl::Kernel(setupData.program,"maxArray"),
+        cl::Kernel(setupData.program,"minArray")
     };
     
     for (int i = 0; i < kernel.size(); ++i)
@@ -90,7 +38,7 @@ void printPlatformsAndDevices(
         kernel[i].setArg(1, tmpBuffer);
     }
 
-    cl::CommandQueue queue(context, device, cl::QueueProperties::Profiling);
+    cl::CommandQueue queue(setupData.context, setupData.device, cl::QueueProperties::Profiling);
     queue.enqueueWriteBuffer(xBuffer, CL_TRUE, 0, sizeof(float) * DIM, x.data());
 
     for (int i = 0; i < kernel.size(); ++i)
@@ -103,10 +51,10 @@ void printPlatformsAndDevices(
         std::cout << result << " ";
     }
     std::cout << std::endl;
-
 }
 
 int main(int argc, char** argv)
 {
-    printPlatformsAndDevices();
+    vec2ScalarExample();
+    return 0;
 }
--- OpenCL-CLHPP-Examples/vec2vec/main.cpp
+++ OpenCL-CLHPP-Examples/vec2vec/main.cpp
@@ -1,88 +1,33 @@
-#define CL_HPP_ENABLE_EXCEPTIONS
-#define CL_HPP_TARGET_OPENCL_VERSION 300
+#include <utils.h>
 
-#include <CL/opencl.hpp>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-
-void printVector(std::vector<float>& v)
-{
-    for (int i = 0; i < v.size(); ++i)
-    {
-        std::cout << v[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
-std::string readFile(const std::string& path)
-{
-    std::ifstream t(path);
-    std::stringstream buffer;
-    buffer << t.rdbuf();
-    return buffer.str();
-}
-
-void printPlatformsAndDevices(
+void vec2vecExample(
     const std::string& platformName = "NVIDIA CUDA",
     const cl_device_type& deviceType = CL_DEVICE_TYPE_GPU,
     const std::string& deviceName = "GeForce GT 730")
 {
-    std::vector<cl::Platform> platforms;
-    cl::Platform::get(&platforms);
-    cl::Platform platform;
+    
+    OpenCLData setupData = basicSetup(
+        platformName,
+        deviceType,
+        deviceName,
+        CL_KERNEL_PATH + std::string("/vadd.cl")
+    );
 
-    for (auto& p : platforms) {
-        const std::string& currentPlatformName = p.getInfo<CL_PLATFORM_NAME>();
-        if (currentPlatformName == platformName)
-        {
-            std::cout << p.getInfo<CL_PLATFORM_NAME>() << std::endl;
-            platform = p;
-        }
-    }
-
-    std::vector<cl::Device> devices;
-    platform.getDevices(deviceType, &devices);
-    cl::Device device;
-    for (auto& d : devices)
-    {
-        const std::string& currentDeviceName = d.getInfo<CL_DEVICE_NAME>();
-        if (deviceName == currentDeviceName)
-        {
-            std::cout << d.getInfo<CL_DEVICE_NAME>() << std::endl;
-            device = d;
-        }
-    }
-
-    const cl::string& kernelSource = readFile(CL_KERNEL_PATH + std::string("/vadd.cl"));
-
-    cl::Context context(device);
-    cl::Device d = context.getInfo<CL_CONTEXT_DEVICES>()[0];
-    std::cout << d.getInfo<CL_DEVICE_NAME>() << std::endl;
-    cl::Program program(context, kernelSource);
-    auto buildResult = program.build(device);
-
-    if (buildResult != CL_SUCCESS)
-    {
-        std::cout << "Cannot build program" << std::endl;
-        exit(1);
-    }
-
-    int DIM = 4;
-    cl::Buffer aBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * DIM);
-    cl::Buffer bBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * DIM);
-    cl::Buffer cBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * DIM);
+    const int DIM = 4;
+    cl::Buffer aBuffer(setupData.context, CL_MEM_READ_ONLY, sizeof(float) * DIM);
+    cl::Buffer bBuffer(setupData.context, CL_MEM_READ_ONLY, sizeof(float) * DIM);
+    cl::Buffer cBuffer(setupData.context, CL_MEM_WRITE_ONLY, sizeof(float) * DIM);
 
     std::vector<float> a{1.0,2.0,3.0,4.0};
     std::vector<float> b{ 4.0,3.0,2.0,1.0 };
     std::vector<float> c(DIM);
 
-    cl::Kernel kernel(program, "vadd");
+    cl::Kernel kernel = cl::Kernel(setupData.program, "vadd");
     kernel.setArg(0, aBuffer);
     kernel.setArg(1, bBuffer);
     kernel.setArg(2, cBuffer);
 
-    cl::CommandQueue queue(context, device, cl::QueueProperties::Profiling);
+    cl::CommandQueue queue(setupData.context, setupData.device, cl::QueueProperties::Profiling);
     queue.enqueueWriteBuffer(aBuffer, CL_TRUE, 0, sizeof(float) * DIM, a.data());
     queue.enqueueWriteBuffer(bBuffer, CL_TRUE, 0, sizeof(float) * DIM, b.data());
 
@@ -99,5 +44,6 @@ void printPlatformsAndDevices(
 
 int main(int argc, char** argv)
 {
-    printPlatformsAndDevices();
+    vec2vecExample();
+    return 0;
 }
